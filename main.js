d46fae0617408bdf79083c420fd7ce51917f9956
alia-ouerghui <aliaouerghui4@gmail.com>
2021-10-14 16:34:30 +0100

    third commit

--- main.js
+++ main.js
@@ -2,4 +2,26 @@ var modal = $('Btn');
 window.onclick = function(event) {
 $('Btn').hide()
 }
-
+function refresh(){
+    location.reload();
+    }
+    $("#por1").hide()
+    $("#por2").hide()
+    $("#por3").hide()
+    $("#por4").hide()
+    function checkPlants() {
+        $( "#" ).click(function(){
+        if($("#p1").prop("checked")){
+             $("#").show("")
+        }
+     else if($("#p2").prop("checked")){
+            $("#").show("")
+        }
+      else if($("#p3").prop("checked")){
+            $("#").show("")
+    }
+  else  if($("#p1").prop("checked")){
+        $("#").show("")
+  }
+  else if()
+}
