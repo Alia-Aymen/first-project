6cf9dca40790a226efaa742fa2fe065c739cb2c2
alia-ouerghui <aliaouerghui4@gmail.com>
2021-10-15 15:30:27 +0100

    commit 4

--- main.js
+++ main.js
@@ -1,27 +1,48 @@
-var modal = $('Btn');
-window.onclick = function(event) {
-$('Btn').hide()
-}
+
 function refresh(){
-    location.reload();
-    }
-    $("#por1").hide()
-    $("#por2").hide()
-    $("#por3").hide()
-    $("#por4").hide()
-    function checkPlants() {
-        $( "#" ).click(function(){
-        if($("#p1").prop("checked")){
-             $("#").show("")
-        }
-     else if($("#p2").prop("checked")){
-            $("#").show("")
-        }
-      else if($("#p3").prop("checked")){
-            $("#").show("")
-    }
-  else  if($("#p1").prop("checked")){
-        $("#").show("")
+  location.reload();
   }
-  else if()
+  $("#por1").hide()
+  $("#por2").hide()
+  $("#por3").hide()
+  $("#por4").hide()
+  function checkPlants()
+  {  
+      if($("#check1").prop("checked")){
+           $("#por1").show()
+      }
+   else if($("#check2").prop("checked")){
+          $("#por2").show()
+      }
+    else if($("#check3").prop("checked")){
+          $("#por3").show()
+  }
+else  if($("#check4").prop("checked")){
+      $("#por4").show()
 }
+else if($("#check1").prop("checked") === false && $("#check2").prop("checked")=== false && $("#check3").prop("checked") === false && $("#check4").prop("checked") === false ){
+    alert("you should fil them all")
+ }
+}
+$("#sub").click(function(){
+  $("#inp").hide();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
